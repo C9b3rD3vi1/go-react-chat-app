75d2bfc866b687739211bb05606259b45163970f
C9b3rD3vi1 <nicksonwe@gmail.com>
2024-10-23 17:21:06 +0300

    coding

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ func main() {
     app := fiber.New()
 
     app.Get("/", func (c *fiber.Ctx) error {
-        return c.SendString("Hello, World!")
+        return c.SendString("Hello, World!, Welcome to fiber chat server")
     })
 
     log.Fatal(app.Listen(":3000"))
