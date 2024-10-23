e7ff287a2e7186bb872552210e34a309db2a76b4
C9b3rD3vi1 <nicksonwe@gmail.com>
2024-10-23 16:36:06 +0300

    server initialization

---
+++ main.go
@@ -0,0 +1,30 @@
+
+package main
+
+import "fmt"
+
+import (
+    "log"
+    //"net/http"
+    
+)
+
+import (
+    "github.com/gofiber/fiber/v2"
+	//"github.com/gofiber/websocket/v2"
+)
+
+
+
+// WebSocket server
+// Main chat application function
+func main() {
+	fmt.Println("Starting.....")
+    app := fiber.New()
+
+    app.Get("/", func (c *fiber.Ctx) error {
+        return c.SendString("Hello, World!")
+    })
+
+    log.Fatal(app.Listen(":3000"))
+}
